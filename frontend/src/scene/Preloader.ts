840b88e2b89ca1b4bbac9b2223da6fadd03d2594
myoan <motoki.yoan@gmail.com>
2022-03-09 21:04:45 +0900

    Enable to update gameserver info

--- frontend/src/scene/Preloader.ts
+++ frontend/src/scene/Preloader.ts
@@ -6,8 +6,8 @@ export default class Preloader extends Phaser.Scene {
   id: String;
   graphics: Phaser.GameObjects.Graphics;
   conn: WebSocket;
-  dotNum: integer;
-  tick: integer;
+  ip: string;
+  port: integer;
 
   constructor(id: String, score: integer) {
     super('preloader');
@@ -19,52 +19,53 @@ export default class Preloader extends Phaser.Scene {
     const content = [
       `ID: ${id}`,
       `Score: ${score}`,
-      "[ENTER] -> Game Start!!"
+      "[ENTER] -> Game Start"
     ]
     this.id = id;
 
     text = this.add.text(100, 100, content, { fontFamily: 'Arial', color: '#00ff00' });
-    console.log('get api.snake-game.myoan.dev/room');
+    this.input.keyboard.on('keydown-ENTER', () => { this.connect() }, this);
+  }
 
+  connect() {
     const scene = this;
+    console.log('get api.snake-game.myoan.dev/room');
 
     axios.get('http://api.snake-game.myoan.dev/room')
       .then(function (resp) {
-        console.log(resp);
         const data = resp.data;
 
-        scene.input.keyboard.on('keydown-ENTER', () => { scene.connect(data.ip, data.port) }, scene);
+        scene.ip = data.ip;
+        scene.port = data.port;
+
+        console.log("connect " + scene.ip + ':' + scene.port);
+        scene.conn = new WebSocket('ws://' + scene.ip + ":" + scene.port + '/');
+
+        scene.conn.onmessage = (event) => {
+          const data = JSON.parse(event.data);
+          switch(data.status) {
+            case 0: // GameStatusInit
+              scene.id = data.id
+              break;
+
+            case 1: // GameStatusOk
+              scene.scene.start('game', [scene.id, scene.conn])
+              break
+
+            case 3: // GameStatusWaiting...
+              console.log(`waiting ...`)
+
+              text.destroy();
+              text = scene.add.text(100, 100, "waiting...", { fontFamily: 'Arial', color: '#00ff00' });
+              break;
+
+            default:
+              console.log(`data(${data.status}): ${data}`)
+          }
+        };
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
-
-  connect(ip: String, port: integer) {
-    console.log("connect " + ip + port.toString());
-    this.conn = new WebSocket('ws://' + ip + ":" + port.toString() + '/');
-
-    this.conn.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      switch(data.status) {
-        case 0: // GameStatusInit
-          this.id = data.id
-          break;
-
-        case 1: // GameStatusOk
-          this.scene.start('game', [this.id, this.conn])
-          break
-
-        case 3: // GameStatusWaiting...
-          console.log(`waiting ...`)
-
-          text.destroy();
-          text = this.add.text(100, 100, "waiting...", { fontFamily: 'Arial', color: '#00ff00' });
-          break;
-
-        default:
-          console.log(`data(${data.status}): ${data}`)
-      }
-    };
   }
 }
