a746b28f353a3b1969424d85986b7de75613c516
myoan <motoki.yoan@gmail.com>
2022-03-15 18:03:19 +0900

    Divide webpack config to prod and dev
    It decrease bundle.js size and download time

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,14 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// const pathToPhaser = path.join(__dirname, '/mode_modules/pahser/');
-// const phaser = path.join(pathToPhaser, 'dist/phaser.js');
-
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
   entry: './app.ts',
   watch: false,
   output: {
@@ -16,15 +10,7 @@ module.exports = {
     filename: 'bundle.js',
   },
   resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: ['.ts', '.tsx', '.js'],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist')
-    },
-    compress: false,
-    port: 8081,
   },
   plugins: [
     new CleanWebpackPlugin({verbose: true}),
@@ -32,7 +18,6 @@ module.exports = {
   ],
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       {test: /\.tsx?$/, loader: 'ts-loader'},
     ],
   },
