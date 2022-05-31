db8cb45698c4ea7133765a3b02b71b324281170a
myoan <motoki.yoan@gmail.com>
2022-06-01 00:41:56 +0900

    Alias func

--- gameserver/game.go
+++ gameserver/game.go
@@ -68,24 +68,30 @@ type Client interface {
 
 type Scene struct {
 	ID       int
-	eventMap map[int]func(args interface{})
+	eventMap map[int]SceneHandler
 }
 
-func (s *Scene) AddEventHandler(eventType int, f func(interface{})) error {
+func (s *Scene) AddEventHandler(eventType int, h SceneHandler) error {
 	fn := s.eventMap[eventType]
 	if fn != nil {
 		return fmt.Errorf("scene ID:'%d' already exists", eventType)
 	}
-	s.eventMap[eventType] = f
+	s.eventMap[eventType] = h
 	return nil
 }
+
+type SceneHandler func(interface{})
 
 type SceneManager struct {
 	// SceneID is current scene ID
 	SceneID        int
 	sceneMap       map[int]func(args interface{})
 	Scenes         []*Scene
-	defaultHandler func(args interface{})
+	defaultHandler SceneHandler
+}
+
+func (h SceneHandler) handler(i interface{}) {
+	h(i)
 }
 
 func NewSceneManager() *SceneManager {
@@ -113,19 +119,19 @@ func (mng *SceneManager) FindBySceneID(sceneID int) (*Scene, error) {
 // AddHandler set function which called when server is selected scene and selected event is occurred
 // If selected scene or event is not found, SceneManagaer call default handler and it do nothing.
 // If you want to change default handler, you can use SceneManager.DefaultHandler(f)
-func (mng *SceneManager) AddHandler(eventType int, sceneID int, f func(interface{})) error {
+func (mng *SceneManager) AddHandler(eventType int, sceneID int, h SceneHandler) error {
 	scene, err := mng.FindBySceneID(sceneID)
 	if err != nil {
 		mng.addScene(sceneID)
 		scene, _ = mng.FindBySceneID(sceneID)
 	}
-	scene.AddEventHandler(eventType, f)
+	scene.AddEventHandler(eventType, h)
 	return nil
 }
 
 // DefaultHandler set default handler which called when selected scene and selected event is not found
-func (mng *SceneManager) DefaultHandler(f func(interface{})) {
-	mng.defaultHandler = f
+func (mng *SceneManager) DefaultHandler(h SceneHandler) {
+	mng.defaultHandler = h
 }
 
 func (mng *SceneManager) Update(data interface{}) error {
@@ -150,8 +156,7 @@ func (mng *SceneManager) MoveScene(sid int) {
 }
 
 func (mng *SceneManager) addScene(sceneID int) {
-	log.Printf("AddScene")
-	m := make(map[int]func(interface{}))
+	m := make(map[int]SceneHandler)
 	scene := &Scene{
 		ID:       sceneID,
 		eventMap: m,
